6febaa522a3cfc1f2a9125fe072376fe8cd75887
palimedia <info@palimedia.at>
2023-11-13 21:02:32 +0100

    m

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,6 +3,9 @@ sap.ui.define(["./BaseController"], function (BaseController) {
 
 	return BaseController.extend("com.palimedia.app.controller.App", {
 		onInit: function () {
+			// set log level
+			sap/base/Log.setLevel(sap/base/Log.Level.NONE);
+
 			// apply content density mode to root view
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 		}
